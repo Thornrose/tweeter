47d3fcfa8d6b578a1494197119710dc68f2a295f
Cheever Esler <ames.esler@gmail.com>
2022-11-08 22:19:56 -0400

    character counter working

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,17 @@
+
+console.log("composer-char-counter script loaded successfully");
+
+
+$(document).ready(function() {
+  
+
+  $("#tweet-text").on("input", function(event) {
+
+    const maximumCharacters = 140;
+    const counterElement = this.nextElementSibling.children[1];
+
+    counterElement.value = maximumCharacters - this.value.length; // instead of accessing 140 directly, this seems like better practice
+  });
+
+});
+
