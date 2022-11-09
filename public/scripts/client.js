c16bb51f8ef983c473f5f5494b601656f95f59bf
Cheever Esler <ames.esler@gmail.com>
2022-11-09 16:08:05 -0400

    started dynamic tweet render, more light css tweaking

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,89 @@
 /*
  * Client-side JS logic goes here
  * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
+ * Reminder: Use (and do all your DOM work in) jQuery's document ready function (!!!)
  */
 
+
+// // test for createTweetElement
+// const testTweet = {
+//   "user": {
+//     "name": "Newton",
+//     "avatars": "https://i.imgur.com/73hZDYK.png",
+//     "handle": "@SirIsaac"
+//   },
+//   "content": {
+//     "text": "If I have seen further it is by standing on the shoulders of giants"
+//   },
+//   "created_at": 1667828947053
+// }
+
+// const $tweet = (createTweetElement(testTweet));
+// console.log($tweet);
+
+// // $(document).ready(function() {
+// //   $('#container').append($tweet);
+// // });
+
+
+// test for renderTweets:
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+
+$(document).ready(function() {
+
+  const createTweetElement = function(tweet) {
+    let $tweet = `
+    <article class="tweet">
+      <header>
+        <div class="tweet-avatar"><img src="${tweet.user.avatars}"> ${tweet.user.name}</div>
+        <div class="tweet-handle">${tweet.user.handle}</div>
+      </header>
+      <p>${tweet.content.text}</p>
+      <footer>
+        <div class="tweet-date">${tweet.created_at}</div>
+        <div class="tweet-icons">
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-solid fa-retweet"></i>
+          <i class="fa-solid fa-heart"></i>
+        </div>
+      </footer>
+    </article>
+    `;
+  
+    return $tweet;
+  };
+  
+  const renderTweets = function(tweetArray) { // array of tweet objects
+    for (const tweet of tweetArray) {
+      const $newTweet = createTweetElement(tweet);
+      $('#container').append($newTweet);
+    }
+  }
+
+
+  renderTweets(data);
+});
