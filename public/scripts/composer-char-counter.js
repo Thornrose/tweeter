afe98b6b9c761cfd22234c0f92957397ee127b31
Cheever Esler <ames.esler@gmail.com>
2022-11-08 22:53:38 -0400

    output style change setup

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -11,6 +11,14 @@ $(document).ready(function() {
     const counterElement = this.nextElementSibling.children[1];
 
     counterElement.value = maximumCharacters - this.value.length; // instead of accessing 140 directly, this seems like better practice
+
+    if (counterElement.value < 0) {   // if statements to change CSS
+      counterElement.classList.add("tweet-invalid");
+    }
+    if (counterElement.value >= 0) {
+      counterElement.classList.remove("tweet-invalid");
+    }
+
   });
 
 });
